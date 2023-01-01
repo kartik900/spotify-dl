d54793baaa469c3af7e228d282463a61f68a92c8
karthik <karthikundi90@gmail.com>
2023-01-01 17:00:43 +0530

    change class order

--- spotify-dl/models/UserPlaylistData.cs
+++ spotify-dl/models/UserPlaylistData.cs
@@ -1,36 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace spotify_dl.models
+﻿namespace spotify_dl.models
 {
-    public class ExternalUrls
-    {
-        public string? Spotify { get; set; }
-    }
-
-
-    public class Item
-    {
-        public string? Id { get; set; }
-        public string? Name { get; set; }
-        public Tracks? Tracks { get; set; }
-    }
-
-    public class UserPlaylistData
+    internal class UserPlaylistData
     {
         public string? Href { get; set; }
         public List<Item>? Items { get; set; }
         public int Total { get; set; }
     }
-
-    public class Tracks
+    internal class Item
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public Tracks? Tracks { get; set; }
+    }
+    internal class Tracks
     {
         public string? Href { get; set; }
         public int Total { get; set; }
     }
-
-
+    internal class ExternalUrls
+    {
+        public string? Spotify { get; set; }
+    }
 }
